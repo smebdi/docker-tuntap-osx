61906bee6dfe598159ad92588dbf361177def327
Caleb Davenport <xcmdprompt@gmail.com>
2020-06-22 18:04:42 -0500

    Update docker_tap_up.sh

--- sbin/docker_tap_up.sh
+++ sbin/docker_tap_up.sh
@@ -8,9 +8,10 @@ localTapInterface=tap1
 hostTapInterface=eth1
 
 # Local and host gateway addresses
-localGateway='10.0.75.1/30'
-hostGateway='10.0.75.2'
-hostNetmask='255.255.255.252'
+# Match local to docker, host to local
+localGateway='10.10.0.1/24'
+hostGateway='10.10.0.2'
+hostNetmask='255.255.255.0'
 
 # Startup local and host tuntap interfaces
 sudo ifconfig $localTapInterface $localGateway up
